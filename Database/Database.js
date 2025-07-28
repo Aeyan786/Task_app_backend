038aa449279b8b6f50d6c283666cfa18fe648678
aeyan34 <aeyanraza34@gmail.com>
2025-07-28 12:53:15 -0700

    add files

--- Database/Database.js
+++ Database/Database.js
@@ -3,7 +3,15 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const MONGODB_URI = process.env.MONGODB_URI;
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+      serverSelectionTimeoutMS:10000,
+      socketTimeoutMS:45000,
+      maxPoolSize:10,
+      minPoolSize:1,
+      bufferCommands:false
+    });
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
